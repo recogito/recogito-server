32ebe8cb28bcbe055f68800a58aa6d6c7ad36511
lorinjameson <lwjameson@gmail.com>
2024-01-10 10:02:31 -0500

    Migrations error

--- supabase/migrations/20240110145656_fix_remotes.sql
+++ supabase/migrations/20240110145656_fix_remotes.sql
@@ -1,5 +1,3 @@
-create extension if not exists "pg_cron" with schema "extensions";
-
 drop policy "Users with correct policies can DELETE on documents" on "public"."documents";
 
 drop policy "Users with correct policies can INSERT on documents" on "public"."documents";
@@ -7,7 +5,6 @@ drop policy "Users with correct policies can INSERT on documents" on "public"."d
 drop policy "Users with correct policies can SELECT on documents" on "public"."documents";
 
 drop policy "Users with correct policies can UPDATE on documents" on "public"."documents";
-
 
 create policy "Users with correct policies can DELETE on documents"
 on "public"."documents"
@@ -40,4 +37,6 @@ for update
 to authenticated
 using (((((is_private = false) OR (created_by = auth.uid()) OR is_admin_organization(auth.uid())) AND check_action_policy_organization(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types)) OR check_action_policy_project_from_document(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types, id) OR check_action_policy_layer_from_document(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types, id)))
 with check (((((is_private = false) OR (created_by = auth.uid()) OR is_admin_organization(auth.uid())) AND ((collection_id IS NULL) OR is_admin_organization(auth.uid())) AND check_action_policy_organization(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types)) OR check_action_policy_project_from_document(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types, id) OR check_action_policy_layer_from_document(auth.uid(), 'documents'::character varying, 'UPDATE'::operation_types, id)));
+
+
 
