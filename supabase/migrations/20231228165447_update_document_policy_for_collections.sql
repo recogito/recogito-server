3927c587a23a46101393c091d719d04f4258cac9
lorinjameson <lwjameson@gmail.com>
2024-01-12 10:59:31 -0500

    Migration fix

--- supabase/migrations/20231228165447_update_document_policy_for_collections.sql
+++ supabase/migrations/20231228165447_update_document_policy_for_collections.sql
@@ -1,3 +1,15 @@
+CREATE TYPE IF NOT EXISTS activation_types AS ENUM('cron', 'direct_call');
+
+CREATE TABLE IF NOT EXISTS public.extensions (
+    id uuid NOT NULL DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    created_at timestamp WITH TIME ZONE DEFAULT NOW(),
+    created_by uuid REFERENCES public.profiles,
+    updated_at timestamptz,
+    updated_by uuid REFERENCES public.profiles,
+    activation_type activation_types NOT NULL,
+    metadata json
+);
+
 CREATE TABLE IF NOT EXISTS public.collections (
     id uuid NOT NULL DEFAULT uuid_generate_v4 () PRIMARY KEY,
     created_at timestamp WITH TIME ZONE DEFAULT NOW(),
