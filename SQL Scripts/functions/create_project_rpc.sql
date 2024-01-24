d91818ce79dde4c9a4c6b8b18054b380baa6bb8e
lorinjameson <lwjameson@gmail.com>
2024-01-24 13:43:44 -0500

    WIP

---
+++ SQL Scripts/functions/create_project_rpc.sql
@@ -0,0 +1,27 @@
+CREATE
+OR REPLACE FUNCTION create_project_rpc (
+  _name VARCHAR,
+  _description VARCHAR,
+  _is_open_join BOOLEAN,
+  _is_open_edit BOOLEAN
+) RETURNS SETOF public.projects AS $body$
+DECLARE
+    _project_id uuid := gen_random_uuid();    -- The id of the new project
+    _context_id uuid := gen_random_uuid();    -- The id of the default context
+    _default_context_definition_id uuid;
+BEGIN
+    IF NOT check_action_policy_organization(auth.uid(), 'projects', 'INSERT') THEN
+        RETURN;
+    END IF;    
+
+    INSERT INTO public.projects (id, created_by, created_at, name, description, is_open_join, is_open_edit) VALUES (_project_id, auth.uid(), NOW(), _name, _description, _is_open_join, _is_open_edit);
+
+    INSERT INTO public.contexts (id, created_by, created_at, project_id) VALUES (_context_id, auth.uid(), NOW(), _project_id);
+
+    SELECT (id) INTO _default_context_definition_id FROM public.tag_definitions t WHERE t.scope = 'system' AND t.name = 'DEFAULT_CONTEXT';
+
+    INSERT INTO public.tags (created_by, created_at, tag_definition_id, target_id) VALUES (auth.uid(), NOW(), _default_context_definition_id, _context_id);    
+    
+    RETURN QUERY SELECT * FROM public.projects WHERE id = _project_id;
+END
+$body$ LANGUAGE plpgsql SECURITY DEFINER;
--- SQL Scripts/tables/projects.sql
+++ SQL Scripts/tables/projects.sql
@@ -1,29 +1,36 @@
-CREATE TABLE public.projects
-(
-    id                uuid NOT NULL            DEFAULT uuid_generate_v4() PRIMARY KEY,
-    created_at        timestamp WITH TIME ZONE DEFAULT NOW(),
-    created_by        uuid REFERENCES public.profiles,
-    updated_at        timestamptz,
-    updated_by        uuid REFERENCES public.profiles,
-    is_archived       bool                     DEFAULT FALSE,
-    name              varchar,
-    description       varchar,
-    is_system_project bool                     DEFAULT FALSE
-);
+CREATE TABLE
+    public.projects (
+        id UUID NOT NULL DEFAULT uuid_generate_v4 () PRIMARY KEY,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+        created_by UUID REFERENCES public.profiles,
+        updated_at timestamptz,
+        updated_by UUID REFERENCES public.profiles,
+        is_archived bool DEFAULT FALSE,
+        NAME VARCHAR,
+        description VARCHAR,
+        is_open_join BOOLEAN DEFAULT FALSE,
+        is_open_edit BOOLEAN DEFAULT FALSE
+    );
 
 -- Changes 04/21/23 --
 ALTER TABLE public.projects
-    ADD COLUMN is_system_project bool DEFAULT FALSE;
+ADD COLUMN is_system_project bool DEFAULT FALSE;
 
 -- Changes 05/01/23 --
 ALTER TABLE public.projects
-    DROP COLUMN is_system_project;
+DROP COLUMN is_system_project;
 
 -- Changes 5/24/23 --
 ALTER TABLE public.projects
-    ADD CONSTRAINT projects_created_by_fkey FOREIGN KEY (created_by) REFERENCES public.profiles (id);
+ADD CONSTRAINT projects_created_by_fkey FOREIGN KEY (created_by) REFERENCES public.profiles (id);
 
 -- Changes 7/26/23 --
 ALTER TABLE public.projects
-    ADD COLUMN is_archived bool DEFAULT FALSE;
+ADD COLUMN is_archived bool DEFAULT FALSE;
 
+-- Changes 1/23/24
+ALTER TABLE public.projects
+ADD COLUMN is_open_join BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE public.projects
+ADD COLUMN is_open_edit BOOLEAN DEFAULT FALSE;
