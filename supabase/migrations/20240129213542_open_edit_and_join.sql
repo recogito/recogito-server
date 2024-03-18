8a845790008580e819a33c07764ab6f4d1e5701d
lorinjameson <lwjameson@gmail.com>
2024-03-18 11:43:29 -0400

    Fix for prod

--- supabase/migrations/20240129213542_open_edit_and_join.sql
+++ supabase/migrations/20240129213542_open_edit_and_join.sql
@@ -4,9 +4,9 @@ drop policy "Users with correct policies can SELECT on projects" on "public"."pr
 
 alter table "public"."contexts" add column IF NOT EXISTS "is_project_default" boolean default false;
 
-alter table "public"."projects" add column "is_open_edit" boolean default false;
+alter table "public"."projects" add column IF NOT EXISTS "is_open_edit" boolean default false;
 
-alter table "public"."projects" add column "is_open_join" boolean default false;
+alter table "public"."projects" add column IF NOT EXISTS "is_open_join" boolean default false;
 
 set check_function_bodies = off;
 
