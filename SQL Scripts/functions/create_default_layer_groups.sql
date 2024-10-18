29abe27e7cc4e72e780a06154bf3312d5b6f48c3
lorinjameson <lwjameson@gmail.com>
2024-10-18 15:16:25 -0400

    Migration for locking legacy projects

--- SQL Scripts/functions/create_default_layer_groups.sql
+++ SQL Scripts/functions/create_default_layer_groups.sql
@@ -7,15 +7,17 @@ DECLARE
     _description    varchar;
     _is_admin       bool;
     _is_default     bool;
+    _is_read_only   bool;
 BEGIN
-    FOR _role_id, _name, _description, _is_admin, _is_default IN SELECT role_id, name, description, is_admin, is_default
-                                                    FROM public.default_groups
-                                                    WHERE group_type = 'layer'
+    FOR _role_id, _name, _description, _is_admin, _is_default, _is_read_only 
+        IN SELECT role_id, name, description, is_admin, is_default, is_read_only
+        FROM public.default_groups
+        WHERE group_type = 'layer'
         LOOP
             _layer_group_id = extensions.uuid_generate_v4();
             INSERT INTO public.layer_groups
-                (id, layer_id, role_id, name, description, is_admin, is_default)
-            VALUES (_layer_group_id, NEW.id, _role_id, _name, _description, _is_admin, _is_default);
+                (id, layer_id, role_id, name, description, is_admin, is_default, is_read_only)
+            VALUES (_layer_group_id, NEW.id, _role_id, _name, _description, _is_admin, _is_default, _is_read_only);
 
             IF _is_admin IS TRUE AND NEW.created_by IS NOT NULL THEN
                 INSERT INTO public.group_users (group_type, type_id, user_id)
