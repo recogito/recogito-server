abdebf01d040e54ac1b3369f239efc32a3f5f839
dleadbetter <derek@performantsoftware.com>
2025-01-07 16:12:37 -0500

    #141 - Updating the names of the COVE collections to "COVE Texts" and "COVE Images"

---
+++ supabase/migrations/20250107210556_rename_cove_collections.sql
@@ -0,0 +1,9 @@
+UPDATE public.collections
+   SET name = 'COVE Texts'
+ WHERE id = '2a8279d6-feab-4cd8-b7b0-504d2893dd4a'
+;
+
+UPDATE public.collections
+   SET name = 'COVE Images'
+ WHERE id = 'b0245009-963a-46ba-a8b5-b11add9ceb1a'
+;
