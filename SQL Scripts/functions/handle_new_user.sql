90545254e63e2ad4af36f0faeaf462e4e605a8e4
lorinjameson <lwjameson@gmail.com>
2025-02-19 13:48:00 -0500

    WIP

--- SQL Scripts/functions/handle_new_user.sql
+++ SQL Scripts/functions/handle_new_user.sql
@@ -4,13 +4,22 @@ DECLARE
     _id UUID;
 BEGIN
     RAISE NOTICE 'User Id: %', NEW.id;
-    INSERT INTO public.profiles (id, email)
-    VALUES (NEW.id, NEW.email);
-      IF EXISTS(SELECT 1 FROM public.organization_groups WHERE is_default = TRUE)
-        THEN
-            SELECT id INTO _id FROM public.organization_groups WHERE is_default = TRUE;
-            INSERT INTO public.group_users (user_id, group_type, type_id) VALUES (NEW.id, 'organization', _id);
-        END IF;
+    IF NEW.is_sso_user 
+    AND (NEW.raw_user_meta_data->custom_claims->first_name OR NEW.raw_user_meta_data->custom_claims->last_name)
+    THEN
+        INSERT INTO public.profiles (id, email, first_name, last_name)
+        VALUES (NEW.id, NEW.email, NEW.raw_user_meta_data->custom_claims->first_name, NEW.raw_user_meta_data->custom_claims->last_name);
+    ELSE
+        INSERT INTO public.profiles (id, email)
+        VALUES (NEW.id, NEW.email);
+    END IF;
+
+    
+    IF EXISTS(SELECT 1 FROM public.organization_groups WHERE is_default = TRUE)
+    THEN
+        SELECT id INTO _id FROM public.organization_groups WHERE is_default = TRUE;
+        INSERT INTO public.group_users (user_id, group_type, type_id) VALUES (NEW.id, 'organization', _id);
+    END IF;
     RETURN new;
 END;
 $$;
