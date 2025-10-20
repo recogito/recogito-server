1ed35ae6a8e3f0a604340abb96696f1fcb6c82d8
Ben Silverman <ben@performantsoftware.com>
2025-10-20 16:52:01 -0400

    Allow importing docs from project to collection

---
+++ SQL Scripts/functions/copy_documents_to_collection_rpc.sql
@@ -0,0 +1,61 @@
+CREATE OR REPLACE FUNCTION copy_documents_to_collection_rpc(
+    _collection_id uuid,
+    _document_ids uuid[],
+    _collection_metadata json
+)
+RETURNS TABLE (
+    original_document_id uuid,
+    id uuid,
+    collection_id uuid,
+    name varchar,
+    bucket_id text,
+    content_type content_types_type,
+    collection_metadata json
+)
+AS $body$
+BEGIN
+    RETURN QUERY
+    WITH original_documents AS (
+        SELECT
+            documents.id AS original_document_id,
+            gen_random_uuid() AS new_id,
+            documents.name,
+            documents.bucket_id,
+            documents.content_type,
+            documents.meta_data
+        FROM documents
+        WHERE documents.id = ANY(_document_ids)
+    ),
+    new_documents AS (
+        INSERT INTO documents (
+            id,
+            name,
+            bucket_id,
+            content_type,
+            meta_data,
+            collection_id,
+            is_private,
+            collection_metadata
+        )
+        SELECT
+            od.new_id,
+            od.name,
+            od.bucket_id,
+            od.content_type,
+            od.meta_data,
+            _collection_id,
+            FALSE,
+            _collection_metadata
+        FROM original_documents od
+    )
+    SELECT
+        od.original_document_id,
+        od.new_id,
+        _collection_id,
+        od.name,
+        od.bucket_id,
+        od.content_type,
+        _collection_metadata
+    FROM original_documents od;
+END 
+$body$ LANGUAGE plpgsql SECURITY DEFINER;
